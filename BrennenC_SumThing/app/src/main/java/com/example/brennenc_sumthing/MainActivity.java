aeadd269805100c3d13fb8533bffa6b6990a0229
Terraminx <bccuber@gmail.com>
2019-02-20 11:59:30 -0700

    brighter red

--- BrennenC_SumThing/app/src/main/java/com/example/brennenc_sumthing/MainActivity.java
+++ BrennenC_SumThing/app/src/main/java/com/example/brennenc_sumthing/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.brennenc_sumthing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.AddBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            public void onclick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                float num1 = Float.parseFloat(firstNumEditText.getText().toString());
+                float num2 = Float.parseFloat(firstNumEditText.getText().toString());
+                float result = num1 +num2;
+                resultTextView.setText(result + "");
+
+            }
+        });
     }
 }
